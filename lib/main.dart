2199d07f60ee2aaf381717ea9edf42e10ff6cb16
Sheelove Dol <siesel26@gmail.com>
2024-02-23 00:31:26 -0800

    Added a body and an image widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          "I Am Rich",
-          style: TextStyle(color: Colors.white, fontSize: 30),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text(
+            "I Am Rich",
+            style: TextStyle(color: Colors.white, fontSize: 30),
+          ),
+          backgroundColor: Colors.blueGrey[900],
         ),
-        backgroundColor: Colors.blueGrey[900],
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://imgupscaler.com/images/samples/animal-after.webp'),
+          ),
+        ),
       ),
     ),
-  ));
+  );
 }
